d85a48624abd702eef62be3ccbfd3993f0048552
[신자민] <[sjmim0220@gmail.com]>
2024-03-25 16:10:12 +0900

    First

---
+++ src/Components/Bottom.jsx
@@ -0,0 +1,12 @@
+export default function Bottom() {
+  return (
+    <div className="bottom-background">
+      <div className="bottom-account-input">
+        <input placeholder="Email address"/>
+        <input placeholder="Password"/>
+      </div>
+      <button>Create an account</button>
+      <p>By signing up I agree to the terms of service and privacy policy</p>
+    </div>
+  );
+}
